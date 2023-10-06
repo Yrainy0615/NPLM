591c11cb8ba3ad11c8ea09adc74d630ee3b52485
Yrainy0615 <yrainy0615@outlook.com>
2023-10-06 13:21:38 +0900

    update

--- scripts/registration/registration.py
+++ scripts/registration/registration.py
@@ -11,14 +11,29 @@ from matplotlib import pyplot as plt
 import igl
 import csv
 
-def show_mesh(mesh, keypoints):
+def show_contour(mesh, keypoints):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot_trisurf(mesh.vertices[:, 0], mesh.vertices[:, 1], mesh.vertices[:, 2], triangles=mesh.faces, color='gray', alpha=0.6)
     for i,idx in enumerate(keypoints):
-        point = idx
+        point = mesh.vertices[idx]
+        if i%10==0:
+            ax.scatter3D(point[0], point[1], point[2], c='red', s=5)
+            ax.text(point[0], point[1], point[2], str(i), fontsize=10, color='black')
+
+    ax.set_xlim([-0.5, 0.5])
+    ax.set_ylim([-0.5, 0.5])
+    ax.set_zlim([-0.5, 0.5])
+    plt.show() 
+
+def show_mesh(mesh, keypoints):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_trisurf(mesh.vertices[:, 0], mesh.vertices[:, 1], mesh.vertices[:, 2], triangles=mesh.faces, color='gray', alpha=0.6)
+    for key,idx in keypoints.items():
+        point = mesh.vertices[idx]
         ax.scatter3D(point[0], point[1], point[2], c='red', s=5)
-        ax.text(point[0], point[1], point[2], str(i), fontsize=5, color='black')
+        ax.text(point[0], point[1], point[2], key, fontsize=5, color='black')
 
     ax.set_xlim([-0.5, 0.5])
     ax.set_ylim([-0.5, 0.5])
@@ -40,7 +55,44 @@ def icp_correspondence(template, target, template_contour):
         idx_list.append(original_index)
     return idx_list
 
+def compare_keypoint(mesh1, keypoints1, mesh2, keypoints2):
+    # Create a new figure and 3D axis
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
+    keypoint_color = 'red'
+
+    # Plot the first mesh faces
+    ax.plot_trisurf(mesh1.vertices[:, 0], mesh1.vertices[:, 1], mesh1.vertices[:, 2], triangles=mesh1.faces, color='gray', alpha=0.6)
+
+    # Plot keypoints for the first mesh
+    for key,idx in keypoints1.items():
+        point = mesh1.vertices[idx]
+        ax.scatter3D(point[0], point[1], point[2], c=keypoint_color, s=5)
+        ax.text(point[0], point[1], point[2], key, fontsize=5, color='black')
+        
+    # Plot the second mesh faces
+    ax.plot_trisurf(mesh2.vertices[:, 0], mesh2.vertices[:, 1], mesh2.vertices[:, 2], triangles=mesh2.faces, color='lightblue', alpha=0.6)
+
+    # Plot keypoints for the second mesh
+    for key,idx in keypoints2.items():
+        point = mesh2.vertices[idx]
+        ax.scatter3D(point[0], point[1], point[2], c=keypoint_color, s=5)
+        ax.text(point[0], point[1], point[2], key, fontsize=5, color='black')
+
+
+    # Connect keypoints with same index from both meshes
+    for key in keypoints1.keys():
+        idx1 = keypoints1[key]
+        idx2 = keypoints2[key]
+        point1 = mesh1.vertices[idx1]
+        point2 = mesh2.vertices[idx2]
+        ax.plot3D([point1[0], point2[0]], [point1[1], point2[1]], [point1[2], point2[2]], 'b-') # using blue color for lines
+
+    # Set axis limits
+    ax.set_xlim([-0.5, 0.5])
+    ax.set_ylim([-0.5, 0.5])
+    ax.set_zlim([-0.5, 0.5])
 def compare_mesh(mesh1, keypoints1, mesh2, keypoints2):
     # Create a new figure and 3D axis
     fig = plt.figure()
@@ -53,25 +105,30 @@ def compare_mesh(mesh1, keypoints1, mesh2, keypoints2):
 
     # Plot keypoints for the first mesh
     for i,idx in enumerate(keypoints1):
-        point = mesh1.vertices[idx]
-        ax.scatter3D(point[0], point[1], point[2], c=keypoint_color, s=5)
+        if idx % 10 == 0:
+            point = mesh1.vertices[idx]
+            ax.scatter3D(point[0], point[1], point[2], c=keypoint_color, s=5)
+            ax.text(point[0], point[1], point[2], str(i), fontsize=5, color='black')
+
 
     # Plot the second mesh faces
-    ax.plot_trisurf(mesh2.vertices[:, 0], mesh2.vertices[:, 1], mesh2.vertices[:, 2], triangles=mesh2.faces, color='lightblue', alpha=0.6)
-    ax.text(point[0], point[1], point[2], str(i), fontsize=5, color='black')
+        ax.plot_trisurf(mesh2.vertices[:, 0], mesh2.vertices[:, 1], mesh2.vertices[:, 2], triangles=mesh2.faces, color='lightblue', alpha=0.6)
+     
     # Plot keypoints for the second mesh
     for j,idx in enumerate(keypoints2):
-        point = mesh2.vertices[idx]
-        ax.scatter3D(point[0], point[1], point[2], c=keypoint_color, s=5)
-        ax.text(point[0], point[1], point[2], str(j), fontsize=5, color='black')
+        point = mesh2.vertices[idx]    
+        if idx%10==0:
+            ax.scatter3D(point[0], point[1], point[2], c=keypoint_color, s=5)
+            ax.text(point[0], point[1], point[2], str(j), fontsize=10, color='black')
 
 
     # Connect keypoints with same index from both meshes
     for idx1, idx2 in zip(keypoints1, keypoints2):
-        point1 = mesh1.vertices[idx1]
-        point2 = mesh2.vertices[idx2]
-        ax.plot3D([point1[0], point2[0]], [point1[1], point2[1]], [point1[2], point2[2]], 'b-') # using blue color for lines
-
+        if idx1 % 10 == 0:
+            point1 = mesh1.vertices[idx1]
+            point2 = mesh2.vertices[idx2]
+            ax.plot3D([point1[0], point2[0]], [point1[1], point2[1]], [point1[2], point2[2]], 'b-')
+        
     # Set axis limits
     ax.set_xlim([-0.5, 0.5])
     ax.set_ylim([-0.5, 0.5])
@@ -231,22 +288,37 @@ class LeafRegistration():
      
     def ARAP_rigistration(self,target_path):
         template = self.raw_to_canonical(self.template)  
-        template = self.ICP_rigid_registration(target_path)
+        template.fill_holes()
+        #template = self.ICP_rigid_registration(target_path)
         target = trimesh.load_mesh(target_path)
         target = self.raw_to_canonical(target)
+        target.fill_holes()
         pca_target = self.find_pca(target)
         keypoints_target = self.find_keypoints(target,pca_target)
+        keypoints_temp = {
+            'top':self.vertex_info['top'][0],
+            'base':self.vertex_info['base'][0],
+            'left':self.vertex_info['left'][0],
+            'right':self.vertex_info['right'][0]
+        }
         template_contour = self.vertex_info['contour']
         # get boundary index of target mesh
         target_contour = self.get_boundary(target)
+        keypoints_target = self.get_correspondence(template, target, keypoints_temp, keypoints_target)
+        
         keypoints_target_up = self.sampling_keypoints(target, keypoints_target)
-        
+        keypoints_temp_up = self.sampling_keypoints(template, keypoints_temp)
         keypoints_target_surf = icp_correspondence(target.vertices, keypoints_target_up, template_contour=target_contour)        
+        keypoints_temp_surf = icp_correspondence(template.vertices, keypoints_temp_up, template_contour=template_contour)
         # find correspondence
-        # show_mesh(template, template.vertices[template_contour])
-        # show_mesh(target, target.vertices[keypoints_target_surf])
-        # compare_mesh(template, template_contour, target, keypoints_target_surf)
+        # show_mesh(template, keypoints_temp)
+        # show_mesh(target, keypoints_target)
+        # compare_keypoint(template, keypoints_temp, target, keypoints_target)
         
+        # show contour
+        # show_contour(template, keypoints_temp_surf)
+        # show_contour(target, keypoints_target_surf)    
+       #  compare_mesh(template, template_contour, target, keypoints_target_surf)
         
         
         # Use libigl to perform non-rigid deformation
@@ -265,7 +337,7 @@ class LeafRegistration():
         # Perform ARAP deformation
         vn = arap.solve(bc, v)
         deformed_mesh = trimesh.Trimesh(vertices=vn, faces=f)
-        compare_mesh(deformed_mesh, template_contour, target, keypoints_target_surf)
+        # compare_mesh(deformed_mesh, template_contour, target, keypoints_target_surf)
         return deformed_mesh
     
     def ICP_rigid_registration(self, target_path):
@@ -284,6 +356,6 @@ if __name__ == '__main__':
     template = 'dataset/LeafData/Jamun/healthy/Jamun_healthy_0019.obj'
     vertex_info = 'dataset/vertex.csv'
     registrater  =LeafRegistration(root_dir, template, vertex_info)
-    target = 'dataset/ScanData/poplar_dry/Leaf_poplar_dry.001.obj'
+    target = 'dataset/ScanData/yellow/Leaf_yellow.003.obj'
     registrater.ARAP_rigistration(target)
     #registrater.ICP_rigid_registration(target)
