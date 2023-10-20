1ca311fb5fac801797d3440cf83c67872c847be4
Yrainy0615 <yrainy0615@outlook.com>
2023-10-20 20:01:19 +0900

    update depth

--- scripts/model/renderer.py
+++ scripts/model/renderer.py
@@ -9,19 +9,24 @@ from pytorch3d.renderer import (
     MeshRenderer, 
     MeshRasterizer,  
     SoftPhongShader,
+    PointsRasterizationSettings,
+    PointsRasterizer,
     blending,
-    Textures
+    Textures,
+    PointsRenderer,
+    AlphaCompositor
 )
 from pytorch3d.io import load_objs_as_meshes, load_obj
 import torch
-from pytorch3d.structures import Meshes,  Pointclouds
+from pytorch3d.structures import Meshes,  Pointclouds, Volumes
 from pytorch3d.loss import mesh_laplacian_smoothing
 from matplotlib import pyplot as plt
 import numpy as np
 from pytorch3d.vis.plotly_vis import AxisArgs, plot_batch_individually, plot_scene
 from pytorch3d.ops import add_pointclouds_to_volumes
-
-
+import open3d as o3d
+import mcubes
+from pytorch3d.ops.marching_cubes import marching_cubes_naive
 
 class MeshRender():
     def __init__(self,device):
@@ -50,6 +55,20 @@ class MeshRender():
             blend_params=blend_params
         )
     )
+        
+        raster_settings = PointsRasterizationSettings(
+        image_size=256, 
+        radius = 0.01,
+        points_per_pixel = 5
+    )
+        # PointsRasterizer
+        rasterizer_point = PointsRasterizer(cameras=self.cameras, raster_settings=raster_settings)
+        self.point_renderer = PointsRenderer(
+            rasterizer=rasterizer_point,
+            compositor=AlphaCompositor(background_color=(0,0,0))
+        ).cuda()
+        
+        
     def render_rgb(self, mesh):
         return self.renderer(mesh)
     
@@ -78,30 +97,59 @@ class MeshRender():
         plt.title("Depth Visualization")
         plt.show()
     
-    def depth_pts(self, depth):
-        depth = depth.squeeze()
-        depth[depth == -1] = 0
+    
+    def depth_pointcloud(self, depth):
+        depth = depth.detach().cpu().squeeze().numpy()
+        fov =60
+        width , height = depth.shape[0], depth.shape[1]
+        cx = width / 2
+        cy = height / 2
+        fx = cx / np.tan(fov / 2)
+        fy = cy / np.tan(fov / 2)
 
-        sparse_depth = depth.to_sparse()
-        indices = sparse_depth.indices()
-        values = sparse_depth.values()
-        xy_depth = torch.cat((indices.T, values[..., None]), dim=-1)
-        # normalize xy to [-1,1]
-        # xy_depth[:, 0] -= 128
-        xy_depth[:, 0] /= 128 /2
-        # xy_depth[:, 1] -= 128
-        xy_depth[:, 1] /= 128 /2
+        row = height
+        col = width
+        # TODO check whether u or v is the column. depth[v, u] ???
+        v, u = np.indices((row, col))
 
-#points = cameras.unproject_points(xy_depth)
-        pts = self.cameras.unproject_points(xy_depth, world_coordinates=True)
-        return pts
+        # Calculate X_ and Y_ coordinates
+        X_ = (u - cx) / fx
+        Y_ = (v - cy) / fy * depth
+
+        # Create a mask to exclude infinity values from depth
+        mask = depth > -1
+
+        # Apply mask to X_, Y_, and depth
+        X_ = X_[mask]
+        Y_ = Y_[mask]
+        depth_ = depth[mask]
+
+        X = X_ * depth_
+        Y = Y_ * depth_
+        Z = depth_
+
+        coords_g = np.stack([X, Y, Z])  # shape: num_points * 3
+        coords_g = torch.tensor(coords_g, dtype=torch.float32, device=self.device)
+        point_cloud = Pointclouds(points=coords_g.permute(1,0).unsqueeze(0), 
+                                  features=torch.rand(1,coords_g.shape[1], 5).to(self.device))
+        return point_cloud
     
     def pts_volume(self, pts):
-        pass
+        initial_volumes = Volumes(features=torch.zeros(1,5,256,256,256),
+                                  densities=torch.zeros(1,1,256,256,256),
+                                  volume_translation=[-0.5, -0.5, -0.5],
+                                  voxel_size=1/256)
+        
+        updated_volumes = add_pointclouds_to_volumes(
+                        pointclouds=pts,
+                        initial_volumes=initial_volumes.to(self.device),
+                        mode='trilinear',)
+        return updated_volumes
+
 
 if __name__ == "__main__":
     device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
-    meshfile = 'dataset/ScanData/maple/Autumn_maple_leaf.005.obj'
+    meshfile = 'dataset/ScanData/Autumn_maple_leaf.001.obj'
     mesh = load_objs_as_meshes([meshfile],device=device)
     renderer  = MeshRender(device=device)
     #fragments = renderer.rasterize(mesh)
@@ -111,12 +159,27 @@ if __name__ == "__main__":
     # renderer.viz_depth(depth)
     #sdf_grid = renderer.depth_sdf(depth)
    # K = renderer.get_intrinsic()
-    #point_cloud = renderer.fast_from_depth_to_pointcloud(depth)
-    point_cloud = renderer.depth_pts(depth)
-    point_cloud=point_cloud.view(-1,3)
+    point_cloud = renderer.depth_pointcloud(depth)
+   # images_PC = renderer.point_renderer(point_cloud)
+   # images_PC = images_PC.detach().cpu().squeeze(0).numpy()
+    volume = renderer.pts_volume(point_cloud)
+    coords = volume.get_coord_grid().view(1,-1,3).detach().cpu().numpy()
+   # point_cloud = renderer.depth_pts(depth)
+    #point_cloud=point_cloud.view(-1,3)
+   # verts , faces = marching_cubes_naive(coords)
+
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(coords[:, 0], coords[:, 1], coords[:, 2], alpha=0.6, s=1)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    plt.show()
     fig = plot_scene({
     "Pointcloud": {
-        "person": Pointclouds([point_cloud])
+        "person": coords
     }
 })
     fig.show()
