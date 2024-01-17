2416cb8941389ea6ddcd0702cc704a3f0da9820e
Yrainy0615 <yrainy0615@outlook.com>
2024-01-18 02:00:45 +0900

    updated 2dsdf

--- scripts/training/trainer_2dsdf.py
+++ scripts/training/trainer_2dsdf.py
@@ -2,10 +2,12 @@ import torch
 import torch.optim as optim
 import math
 from glob import glob
+import sys
+sys.path.append('NPLM')
+from scripts.model.loss_functions import compute_loss
 import os
 import numpy as np
 import wandb
-from scripts.model.reconstruction import deform_mesh, get_logits, mesh_from_logits,create_grid_points_from_bounds
 from matplotlib import pyplot as plt
 import io
 from PIL import Image
@@ -14,72 +16,41 @@ import wandb
 import warnings
 warnings.filterwarnings("ignore")
 import yaml
-from scripts.dataset.sdf_dataset import LeafShapeDataset
+from scripts.model.renderer import MeshRender
+from scripts.dataset.sdf_dataset import LeafShapeDataset, Leaf2DShapeDataset, LeafSDF2dFDataset
 from scripts.model.fields import SDFNetwork ,UDFNetwork
 from torch.utils.data import DataLoader
-
-
-def save_mesh_image_with_camera(vertices, faces):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.set_facecolor((1, 1, 1))
-    fig.set_facecolor((1, 1, 1))
-    ax.plot_trisurf(vertices[:, 0], vertices[:, 1], faces, vertices[:, 2], shade=True, color='blue')
-
-    ax.view_init(elev=90, azim=180)  
-    ax.dist = 8  
-    ax.set_box_aspect([1,1,1.4]) 
-    ax.set_xlim(-1,1)
-    ax.set_ylim(-1,1)
-    ax.set_zlim(-1,1)
-    plt.axis('off')  
-    
-    buf = io.BytesIO()
-    plt.savefig(buf, format="png", dpi=300)
-    buf.seek(0)
-    img = Image.open(buf)
-    
-    plt.close()
-    return img
-def latent_to_mesh(decoder, latent_idx,device):
-    mini = [-1., -1., -1.]
-    maxi = [1., 1., 1.]
-    grid_points = create_grid_points_from_bounds(mini, maxi, 64)
-    grid_points = torch.from_numpy(grid_points).to(device, dtype=torch.float)
-    grid_points = torch.reshape(grid_points, (1, len(grid_points), 3)).to(device)
-    logits = get_logits(decoder, latent_idx, grid_points=grid_points,nbatch_points=2000)
-    mesh = mesh_from_logits(logits, mini, maxi,64)
-    if mesh in None:
-        return None, None
-    else:
-        img = save_mesh_image_with_camera(mesh.vertices, mesh.faces)
-    return mesh ,img
-  
+import random
+from scripts.dataset.img_to_3dsdf import mesh_from_sdf
+from scripts.model.reconstruction import sdf_from_latent , save_mesh_image_with_camera
+from pytorch3d.structures import Meshes,  Pointclouds, Volumes
 
 class ShapeTrainer(object):
-    def __init__(self, decoder, cfg, trainset,trainloader,device):
+    def __init__(self, decoder, cfg, trainset,trainloader,device,args):
         self.decoder = decoder
         self.cfg = cfg['training']
-        self.latent_idx = torch.nn.Embedding(len(trainset), decoder.lat_dim//2, max_norm = 1.0, sparse=True, device = device).float()
+        self.args =args
+        self.latent_idx = torch.nn.Embedding(len(trainset), decoder.lat_dim, max_norm = 1.0, sparse=True, device = device).float()
         torch.nn.init.normal_(
-            self.latent_idx.weight.data, 0.0, 0.1/math.sqrt(decoder.lat_dim//2)
+            self.latent_idx.weight.data, 0.0, 0.1/math.sqrt(decoder.lat_dim)
         )
-        self.latent_spc = torch.nn.Embedding(trainset.num_neutral, decoder.lat_dim//2, max_norm = 1.0, sparse=True, device = device).float()
-        torch.nn.init.normal_(
-            self.latent_spc.weight.data, 0.0, 0.1/math.sqrt(decoder.lat_dim//2)
-        )   
+        # self.latent_spc = torch.nn.Embedding(len(trainset.all_species), decoder.lat_dim//2, max_norm = 1.0, sparse=False, device = device).float()
+        # torch.nn.init.normal_(
+        #     self.latent_spc.weight.data, 0.0, 0.1/math.sqrt(decoder.lat_dim//2)
+        # )   
         print(self.latent_idx.weight.shape)
-        print(self.latent_spc.weight.shape)
+        # print(self.latent_spc.weight.shape)
         self.trainloader = trainloader
 
         self.device = device
         self.optimizer_decoder = optim.AdamW(params=list(decoder.parameters()),
                                              lr = self.cfg['lr'],
                                              weight_decay= self.cfg['weight_decay'])
-        self.combined_para = list(self.latent_idx.parameters()) + list(self.latent_spc.parameters())
-        self.optimizer_latent = optim.SparseAdam(params= self.combined_para, lr=self.cfg['lr_lat'])
+        #self.combined_para = list(self.latent_idx.parameters()) + list(self.latent_spc.parameters())
+        self.optimizer_latent = optim.SparseAdam(params= self.latent_idx.parameters(), lr=self.cfg['lr_lat'])
         self.lr = self.cfg['lr']
         self.lr_lat = self.cfg['lr_lat']
+        self.renderer = MeshRender(device=device)
 
         self.checkpoint_path = self.cfg['save_path']
         
@@ -145,8 +116,8 @@ class ShapeTrainer(object):
             # for param_group in self.optimizer_lat_val.param_groups:
             #     param_group["lr"] = lr
         
-    def save_checkpoint(self, epoch):
-        path = self.checkpoint_path + '/cgshape_bs8_udf_epoch__{}.tar'.format(epoch)
+    def save_checkpoint(self, epoch,save_name):
+        path = self.checkpoint_path + '/{}__{}.tar'.format(save_name,epoch)
         if not os.path.exists(path):
              torch.save({'epoch': epoch,
                         'decoder_state_dict': self.decoder.state_dict(),
@@ -154,7 +125,7 @@ class ShapeTrainer(object):
                         'optimizer_lat_state_dict': self.optimizer_latent.state_dict(),
                       #  'optimizer_lat_val_state_dict': self.optimizer_lat_val.state_dict(),
                         'latent_idx_state_dict': self.latent_idx.state_dict(),
-                        'latent_spc_state_dict': self.latent_spc.state_dict(),
+                   #     'latent_spc_state_dict': self.latent_spc.state_dict(),
                   
                        # 'latent_codes_val_state_dict': self.latent_codes_val.state_dict()
                        },
@@ -164,7 +135,7 @@ class ShapeTrainer(object):
         self.decoder.train()
         self.optimizer_decoder.zero_grad()
         self.optimizer_latent.zero_grad()
-        loss_dict = compute_loss(batch, self.decoder, self.latent_idx, self.latent_spc,self.device)
+        loss_dict = compute_loss(batch, self.decoder, self.latent_idx, self.device)
         loss_total = 0
         for key in loss_dict.keys():
             loss_total += self.cfg['lambdas'][key] * loss_dict[key]
@@ -175,7 +146,7 @@ class ShapeTrainer(object):
             torch.nn.utils.clip_grad_norm_(self.decoder.parameters(), max_norm=self.cfg['grad_clip'])
 
         if self.cfg['grad_clip_lat'] is not None:
-            torch.nn.utils.clip_grad_norm_(self.combined_para, max_norm=self.cfg['grad_clip_lat'])
+            torch.nn.utils.clip_grad_norm_(self.latent_idx.parameters(), max_norm=self.cfg['grad_clip_lat'])
         self.optimizer_decoder.step()
         self.optimizer_latent.step()
 
@@ -193,6 +164,7 @@ class ShapeTrainer(object):
        # start = self.load_checkpoint()
         start =0
         ckp_interval =self.cfg['ckpt_interval']
+        ckp_vis = self.cfg['ckpt_vis']
         for epoch in range(start, epochs):
             self.reduce_lr(epoch)
             sum_loss_dict = {k: 0.0 for k in self.cfg['lambdas']}
@@ -200,21 +172,29 @@ class ShapeTrainer(object):
             for batch in self.trainloader:
                 loss_dict = self.train_step(batch)
                 loss_values = {key: value.item() if torch.is_tensor(value) else value for key, value in loss_dict.items()}
-  
-                wandb.log(loss_values)
+                if args.use_wandb:
+                    wandb.log(loss_values)
                 
                 for k in loss_dict:
                     sum_loss_dict[k] += loss_dict[k]        
-            if epoch % ckp_interval ==0:
-                self.save_checkpoint(epoch)
-            if epoch %100 ==0:
-                lat = torch.concat([self.latent_idx.weight[5], self.latent_spc.weight[5]]).to(self.device)
-                #_, _, mesh,_,_ = get_mesh_udf_fast(self.decoder, lat.unsqueeze(0))
-                mesh, img = latent_to_mesh(self.decoder, lat,self.device)
-                if img is not None:
-                    wandb.log({'shape': wandb.Image(img)})
-                    mesh.export('sample_result/cgshape_udf_{:04d}.ply'.format(epoch))
-                
+            if epoch % ckp_interval ==0 and epoch >0:
+                self.save_checkpoint(epoch, save_name=CFG['training']['save_name'])
+            
+            if args.save_mesh:
+                if epoch % ckp_vis ==0:
+                    #lat = torch.concat([self.latent_idx.weight[0], self.latent_spc.weight[0]]).to(self.device)
+                    lat = self.latent_idx.weight[random.randint(0,len(trainset)-1)].to(self.device)
+                # _, _ ,mesh_udf = get_mesh_udf_fast(decoder, lat.unsqueeze(0),gradient=False)
+                    #print(mesh_udf)
+                    sdf_3d = sdf_from_latent(self.decoder,lat, 256)
+                    mini = [-.95, -.95, -.95]
+                    maxi = [0.95, 0.95, 0.95]   
+                    mesh = mesh_from_sdf(sdf_3d,mini=mini, maxi=maxi , resolution=256)
+                    # mesh_pytorch = Meshes(verts=torch.from_numpy(mesh.vertices).unsqueeze(0).float(), faces=torch.from_numpy(mesh.faces).unsqueeze(0), textures=None)
+                    # fragments = self.renderer.rasterize(mesh_pytorch.to(self.device))
+                    # img = fragments.pix_to_face.squeeze().cpu().numpy()
+                    mesh.export('epoch_{:04d}.ply'.format(epoch))
+
             n_train = len(self.trainloader)
             for k in sum_loss_dict.keys():
                 sum_loss_dict[k] /= n_train
@@ -225,9 +205,11 @@ class ShapeTrainer(object):
                 
 if __name__ == '__main__':       
     parser = argparse.ArgumentParser(description='RUN Leaf NPM')
-    parser.add_argument('--gpu', type=int, default=3, help='gpu index')
-    parser.add_argument('--wandb', type=str, default='*', help='run name of wandb')
+    parser.add_argument('--gpu', type=int, default=0, help='gpu index')
+    parser.add_argument('--wandb', type=str, default='2d_sdf', help='run name of wandb')
     parser.add_argument('--output', type=str, default='shape', help='output directory')
+    parser.add_argument('--use_wandb', action='store_true', help='use wandb')
+    parser.add_argument('--save_mesh', action='store_true', help='save mesh')
     # setting
 
     args = parser.parse_args()
@@ -237,24 +219,25 @@ if __name__ == '__main__':
 
     config = 'NPLM/scripts/configs/npm.yaml'
     CFG = yaml.safe_load(open(config, 'r'))
-    wandb.init(project='NPLM', name =args.wandb)
-    wandb.config.update(CFG)
-    trainset = LeafShapeDataset(mode='train',
-                        n_supervision_points_face=CFG['training']['npoints_decoder'],
-                        n_supervision_points_non_face=CFG['training']['npoints_decoder_non'],
-                        batch_size=CFG['training']['batch_size'],
-                        sigma_near=CFG['training']['sigma_near'],
-                        root_dir=CFG['training']['root_dir'])
+    
+    if args.use_wandb:
+        wandb.init(project='NPLM', name =args.wandb)
+        wandb.config.update(CFG)
+    #dataset
+    trainset = LeafSDF2dFDataset(root_dir=CFG['training']['root_dir'],
+                                 num_samples=CFG['training']['npoints_decoder'],)
     trainloader = DataLoader(trainset, batch_size=CFG['training']['batch_size'], shuffle=True, num_workers=2)
 
     decoder = UDFNetwork(d_in=CFG['decoder']['decoder_lat_dim'],
                          d_hidden=CFG['decoder']['decoder_hidden_dim'],
                          d_out=CFG['decoder']['decoder_out_dim'],
-                         n_layers=CFG['decoder']['decoder_nlayers'],)
+                         n_layers=CFG['decoder']['decoder_nlayers'],
+                         d_in_spatial=2,
+                         udf_type='sdf')
 
     decoder = decoder.to(device)
-    trainer = ShapeTrainer(decoder, CFG, trainset,trainloader, device)
-    trainer.train(30001)
+    trainer = ShapeTrainer(decoder, CFG, trainset,trainloader, device,args)
+    trainer.train(5001)
             
         
         
